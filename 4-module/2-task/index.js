7e2d91f6eacb91536c54172ed84c4515e3ff2bc9
alnusglutinosa <olga.app2@gmail.com>
2020-11-07 19:48:43 +0200

    4-module

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -3,4 +3,8 @@
  * @return {void}
  */
 function makeDiagonalRed(table) {
+  for (let i = 0; i < table.rows.length; i++) {
+    let tableRow = table.rows[i];
+    tableRow.cells[i].style.backgroundColor = 'red';
+  }
 }
